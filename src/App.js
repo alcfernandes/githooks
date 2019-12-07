6a5ed5beb8a47b2f2ed22df8e7c38987a859de6a
Alessandro Fernandes <alcfernandes@yahoo.com>
2019-12-07 11:59:39 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+
+  // Posso ter vários useEffect um para cada efeito que quero rastrear
+  // Nesse caso o [] indica que esse só será executado uma vez no inicio.
+  useEffect(() => {
+    async function getRepositories() {
+      const response = await fetch(
+        'https://api.github.com/users/alcfernandes/repos'
+      )
+      const data = await response.json()
+      setRepositories(data)
+    }
+    getRepositories()
+  }, [])
+
+  // Esse useEffect só será executado quando o estado de repositories mudar
+  useEffect(() => {
+    const favoritesRepo = repositories.filter(repo => repo.favorite)
+
+    document.title = `Você tem ${favoritesRepo.length} favoritos.`
+  }, [repositories])
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo
+    })
+
+    setRepositories(newRepositories)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ul>
+      {repositories.map(repo => (
+        <li key={repo.id}>
+          {repo.name}
+          {repo.favorite && <span>(Favorito)</span>}
+          <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+        </li>
+      ))}
+    </ul>
+  )
 }
 
-export default App;
+export default App
+
+// function handleAddRepository() {
+//   setRepositories([
+//     ...repositories,
+//     { id: Math.random(), name: 'Novo repositório' }
+//   ])
+// }
